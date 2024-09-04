604710006d9a3399ac24b19068421551778c38fb
essaket <hichamessaket17@gmail.com>
2024-09-04 12:32:30 -0700

    Writing task 0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -9,32 +9,20 @@
 
 const request = require('request');
 
-const getRequest = (url) => {
-  return new Promise((resolve, reject) => {
-    request(url, (error, response, body) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(JSON.parse(body));
-      }
-    });
+const fetch = (url) => new Promise((resolve, reject) => {
+  request(url, (err, _, body) => {
+    if (err) reject(err);
+    else resolve(JSON.parse(body));
   });
-};
+});
 
 const movieId = process.argv[2];
 if (!movieId) {
-  console.error('Usage: 0-starwars_characters.js <movie_id>');
+  console.error('Usage: ./star_wars_characters.js <movie_id>');
   process.exit(1);
 }
-const url = `https://swapi.dev/api/films/${movieId}/`;
 
-getRequest(url)
-  .then((film) => Promise.all(film.characters.map(getRequest)))
-  .then((characters) => {
-    characters.forEach((character) => {
-      console.log(character.name);
-    });
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+fetch(`https://swapi.dev/api/films/${movieId}/`)
+  .then(film => Promise.all(film.characters.map(fetch)))
+  .then(characters => characters.forEach(character => console.log(character.name)))
+  .catch(console.error);
