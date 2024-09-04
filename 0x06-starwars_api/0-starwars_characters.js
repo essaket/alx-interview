21ae393885645a45357a637e66ad72e77c4b7892
essaket <hichamessaket17@gmail.com>
2024-09-04 12:22:23 -0700

    Writing task 0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,33 +3,38 @@
  * Write a script that prints all characters of a Star Wars movie:
  * - The first positional argument passed is the Movie ID - example: 3 = “Return of the Jedi”
  * - Display one character name per line in the same order as the “characters” list in the /films/ endpoint
- * - You must use the Star Wars API
+ * - You must use the Star wars API
  * - You must use the request module
  */
 
-const request = require('request'); // Import the request module
-const movieId = process.argv[2];    // Get the Movie ID from command-line arguments
+const request = require('request');
 
-if (!movieId) {
-  console.error('Usage: ./star_wars_characters.js <movie_id>'); // Print usage if no Movie ID
-  process.exit(1); // Exit with error code
-}
-
-const url = `https://swapi.dev/api/films/${movieId}/`; // SWAPI URL for the given Movie ID
-
-// Fetch movie details
-request(url, (error, response, body) => {
-  if (error) return console.error(error); // Handle error
-
-  const film = JSON.parse(body); // Parse the JSON response
-  const characters = film.characters; // Get the list of character URLs
-
-  // Fetch each character's details
-  characters.forEach((characterUrl) => {
-    request(characterUrl, (error, response, body) => {
-      if (error) return console.error(error); // Handle error
-      const character = JSON.parse(body); // Parse the JSON response
-      console.log(character.name); // Print the character's name
+const getRequest = (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(body));
+      }
     });
   });
-});
+};
+
+const movieId = process.argv[2];
+if (!movieId) {
+  console.error('Usage: 0-starwars_characters.js <movie_id>');
+  process.exit(1);
+}
+const url = `https://swapi.dev/api/films/${movieId}/`;
+
+getRequest(url)
+  .then((film) => Promise.all(film.characters.map(getRequest)))
+  .then((characters) => {
+    characters.forEach((character) => {
+      console.log(character.name);
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
